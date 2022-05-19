ff8058d5c6b6b635b8b145a99e7792eb4b079f32
Rajin <rajink92@gmail.com>
2022-05-19 19:17:04 -0400

    Build and styled DayList and DayListItem

---
+++ src/components/DayList.js
@@ -0,0 +1,13 @@
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  const {days} = props;
+  const dayListData = days.map(day =>{
+    return <DayListItem key={day.key} name={day.name} spots={day.spots} selected={day.name === props.day} setDay={props.setDay}/>
+  });
+  return (
+   <ul>
+     {dayListData}
+   </ul>
+  );
+} 
