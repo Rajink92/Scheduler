493b768956935cf941f06ad26087cc2a4bfb8fd8
Rajin <rajink92@gmail.com>
2022-05-25 16:41:43 -0400

    added Form, create and edit

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,8 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
+
 
 
 storiesOf("Button", module)
@@ -155,3 +157,5 @@ storiesOf("Button", module)
   .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
   .add("Status", () => <Status message="Deleting"/>)
   .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")}/>)
+  .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={(action("onCancel"))}/>)
+  .add("Edit", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={(action("onCancel"))} name="xyz" interviewer="2"/>)
