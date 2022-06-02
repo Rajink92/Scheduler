0f063f1018514b5fef9506220897c47ceda981b8
Rajin <rajink92@gmail.com>
2022-06-02 19:11:21 -0400

    added custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,13 +2,30 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "../../hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appointment(props) {
-
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE"
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment" >
       <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/>: <Empty />}
-    </article>
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer.name}
+      />
+      )}
+      {mode === CREATE && (
+        <Form interviewer={props.interviewer} interviewers={[]} onCancel={() => back(EMPTY)}/>
+      )}
+      
+      </article>
   )
 } 
