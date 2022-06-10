57011802325233dec1888bb3172a9e1bef110d7e
Rajin <rajink92@gmail.com>
2022-06-09 20:11:36 -0400

    added test coverage

--- src/hooks/_tests_/useVisualMode.test.js
+++ src/hooks/_tests_/useVisualMode.test.js
@@ -7,18 +7,18 @@ const SECOND = "SECOND";
 const THIRD = "THIRD";
 
 
-test.skip("useVisualMode should initialize with default value", () => {
+test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   expect(result.current.mode).toBe(FIRST);
 });
-test.skip("useVisualMode should transition to another mode", () => {
+test("useVisualMode should transition to another mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   act(() => result.current.transition(SECOND));
   expect(result.current.mode).toBe(SECOND);
 });
-test.skip("useVisualMode should return to previous mode", () => {
+test("useVisualMode should return to previous mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   act(() => result.current.transition(SECOND));
@@ -34,14 +34,14 @@ test.skip("useVisualMode should return to previous mode", () => {
   expect(result.current.mode).toBe(FIRST);
 });
 
-test.skip("useVisualMode should not return to previous mode if already at initial", () => {
+test("useVisualMode should not return to previous mode if already at initial", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   act(() => result.current.back());
   expect(result.current.mode).toBe(FIRST);
 });
 
-test.skip("useVisualMode should replace the current mode", () => {
+test("useVisualMode should replace the current mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   act(() => result.current.transition(SECOND));
